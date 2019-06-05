3c8302597c53db519e3e6a5274a80d80c92818be
hide6644 <hide6644@gmail.com>
2019-06-05 17:02:09 +0900

    Initial commit

---
+++ src/main/java/crawlerapi/controller/UserController.java
@@ -0,0 +1,24 @@
+package crawlerapi.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import crawlerapi.entity.User;
+import crawlerapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('USER')")
+    public Mono<User> findByUser(@RequestParam("username") String username) {
+        return userService.findByUsername(username);
+    }
+}
