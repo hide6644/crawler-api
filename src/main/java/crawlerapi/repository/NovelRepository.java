f9644004aa9b40945b44c77c7d26ddc859757c95
hide6644 <hide6644@gmail.com>
2023-01-19 20:24:24 +0900

    Initial commit

--- src/main/java/crawlerapi/repository/NovelRepository.java
+++ src/main/java/crawlerapi/repository/NovelRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 import crawlerapi.entity.Novel;
 
-@Repository
 public interface NovelRepository extends JpaRepository<Novel, Long>, JpaSpecificationExecutor<Novel> {
 
     List<Novel> findAllByOrderByTitleAscWriternameAsc();
